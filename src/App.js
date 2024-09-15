b0e9b1c86a36b67a56ab1d269166b90285fcde90
Anderson Dantas <andersondantas016@gmail.com>
2024-09-15 18:52:41 -0300

    agora sim entra as states

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      nome: 'José',
+      contador:0
+    };
+    this.aumentar = this.aumentar.bind(this);
+    this.diminuir = this.diminuir.bind(this);
 }
 
+aumentar(){
+  let state = this.state;
+  state.contador += 1;
+  state.nome="alberi grande jogador do abc"
+  this.setState(state)
+}
+
+diminuir(){
+  let state = this.state;
+  state.contador -= 1;
+  this.setState(state)
+}
+
+
+   render(){
+    return(
+    <div>
+    <h1>{this.state.nome}</h1>
+
+
+
+    <h2>
+    <button onClick={this.diminuir}>-</button>
+        {this.state.contador}
+    <button onClick={this.aumentar}>+</button>
+    </h2>
+
+
+    </div>
+  )
+}
+
+}
 export default App;
